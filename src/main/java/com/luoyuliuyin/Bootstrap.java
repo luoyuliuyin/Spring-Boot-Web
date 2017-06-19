d4d20535d7818c663aaf3e1d23be2f1710637c60
jingfeng <jingfeng@meili-inc.com>
2017-06-19 20:06:23 +0800

    tomcat deploy fix

--- src/main/java/com/luoyuliuyin/Bootstrap.java
+++ src/main/java/com/luoyuliuyin/Bootstrap.java
@@ -2,14 +2,22 @@ package com.luoyuliuyin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by jingfeng on 2017/6/19.
  */
 
 @SpringBootApplication
-public class Bootstrap {
+public class Bootstrap extends SpringBootServletInitializer {
+
     public static void main(String[] args) {
         SpringApplication.run(Bootstrap.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Bootstrap.class);
     }
 }
