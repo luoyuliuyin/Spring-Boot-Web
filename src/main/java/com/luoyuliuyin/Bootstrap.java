41d7d556992d52872c4fb724bdc4297474831419
jingfeng <jingfeng@meili-inc.com>
2017-06-19 16:05:27 +0800

    init

---
+++ src/main/java/com/luoyuliuyin/Bootstrap.java
@@ -0,0 +1,15 @@
+package com.luoyuliuyin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by jingfeng on 2017/6/19.
+ */
+
+@SpringBootApplication
+public class Bootstrap {
+    public static void main(String[] args) {
+        SpringApplication.run(Bootstrap.class, args);
+    }
+}
