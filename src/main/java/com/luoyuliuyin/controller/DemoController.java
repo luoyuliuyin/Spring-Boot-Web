ec67bf4fb932a9eb17d488636ebb149fd1145517
jingfeng <jingfeng@meili-inc.com>
2017-06-22 18:38:55 +0800

    reconstruction

---
+++ src/main/java/com/luoyuliuyin/controller/DemoController.java
@@ -0,0 +1,37 @@
+package com.luoyuliuyin.controller;
+
+import com.alibaba.fastjson.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by jingfeng on 16-6-15.
+ * demo测试
+ */
+
+@Controller
+public class DemoController {
+
+    private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
+
+    @RequestMapping("demo.do")
+    private String demo(HttpServletRequest request, ModelMap modelMap) {
+        logger.info("message", "demo");
+        modelMap.put("message", "demo");
+        modelMap.put("contextPath", request.getContextPath());
+        return "demo.ftl";
+    }
+
+    @ResponseBody
+    @RequestMapping("demo.json")
+    private String demoJson(HttpServletRequest request, ModelMap modelMap) {
+        modelMap.put("message", "demo");
+        return JSON.toJSONString(modelMap);
+    }
+}
---
+++ src/main/resources/templates/demo.ftl
@@ -0,0 +1,9 @@
+<script src="demo.js"></script>
+<script src="jquery-1.10.2.min.js"></script>
+
+<title>demo</title>
+
+<h1>Demo</h1>
+
+<h2>message:${message}</h2>
+<button type="button" onclick="alert_message()">button</button>
